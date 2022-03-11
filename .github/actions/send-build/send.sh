a1fc299137dd66be025ad9759359a0282b8bd6e0
deji <ehinlanwo.deji@live.dk>
2022-03-11 16:18:49 +0100

    removing docker from production

--- .github/actions/send-build/send.sh
+++ .github/actions/send-build/send.sh
@@ -6,7 +6,7 @@ KEY=$4
 
 echo "$KEY" > ./key.pem && chmod 400 ./key.pem
 mv $BUILD $OUTPUT
-sftp -o StrictHostKeyChecking=no -i key.pem $DESTINATION:. <<< $"put -r ${OUTPUT} "
+sudo sftp -o StrictHostKeyChecking=no -i key.pem $DESTINATION:. <<< $"put -r ${OUTPUT} "
 
 if [[ -f  ./key.pem ]]; then
     rm key.pem
